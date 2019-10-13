8a3398ef5166b7d7238e9e8c61faf43767e995cf
imran_buet13 <omeca13@gmail.com>
2019-10-13 22:50:46 +0600

    fixing multiple computer problem

--- family/models.py
+++ family/models.py
@@ -20,3 +20,33 @@ class Child(models.Model):
     childDOB = models.DateField(null=True)
     childPicture = models.ImageField(upload_to='spouse_pic', null=True)
     Family = models.ForeignKey(Family, on_delete=models.SET_NULL, null=True)
+
+
+
+# from django.contrib.auth.models import User
+# from django.db import models
+# from datetime import date
+# # TODO
+
+# # Spouse Email
+# # Spouse Mobile
+# # Picture of Spouse
+# # Picture of Spouse's NID for Verification
+# # Spouse NID
+# # Child Picture
+# class Family(models.Model):
+#     spouse_name = models.CharField(max_length=100, null=True,blank=True )
+#     spouse_date_of_birth =  models.DateField(default=date.today, null=True,blank=True ) 
+#     spouse_email = models.EmailField(null=True,blank=True )
+#     spouse_mobile = models.CharField(max_length=14, null=True, blank=True)
+#     spouse_picture = models.ImageField(upload_to='profile_pics',default='default.jpg', null=True,blank=True )
+    
+#     def __str__(self):
+#         return self.spouse_name
+
+
+# class Child(models.Model):
+#     child_name = models.CharField(max_length=100, null=True,blank=True )
+#     child_date_of_birth =  models.DateField(default=date.today, null=True,blank=True )
+#     child_picture = models.ImageField(upload_to='child_pic',default='default.jpg', null=True,blank=True )
+#     family = models.ForeignKey(Family, on_delete = models.SET_NULL, null=True)
--- family/views.py
+++ family/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render,redirect,get_object_or_404
-from .models import Family
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
+
+from .models import Family
+
 # Create your views here.
 
 @login_required
@@ -17,6 +19,34 @@ def create(request):
         spousename = r.get('spousename')
         childname = r.get('childname')
         family = Family(spousename = spousename, childname = childname)
+        family.save()
+        return redirect('home')
+    return render(request, 'family/create.html')
+
+
+
+# Create your views here.
+
+@login_required
+def home(request):
+    context = {
+        'families': Family.objects.all()
+    }
+    return render(request, 'family/home.html', context)
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        r = request.POST
+        spouse_name = r.get('spouse_name')
+        child_name = r.get('child_name')
+        spouse_date_of_birth = r.get('spouse_date_of_birth')
+        child_date_of_birth = r.get('child_date_of_birth')
+
+        print(spouse_name + " " + child_name + " " + child_date_of_birth + " " + spouse_date_of_birth)
+        family = Family(spouse_name = spouse_name, child_name = child_name, spouse_date_of_birth = spouse_date_of_birth, child_date_of_birth = child_date_of_birth)
+        print(family)
+
         family.save()
         return redirect('home')
     return render(request, 'family/create.html')
