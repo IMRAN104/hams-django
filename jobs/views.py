9429884733391205b9fe3df11647804902d79a0f
imran_buet13 <omeca13@gmail.com>
2019-10-18 10:48:13 +0600

    jobs page is complete, may be needs some polishing of styles

--- jobs/views.py
+++ jobs/views.py
@@ -3,8 +3,14 @@ import operator
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 
+from .models import Job
+
 
 # Create your views here.
 @login_required
 def jobs_home(request):
-    return render(request, 'jobs/jobs_home.html')
+    jobs = Job.objects.all
+    context = {
+        'jobs': jobs
+    }
+    return render(request, 'jobs/jobs_home.html', context)
