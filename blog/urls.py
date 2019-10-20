1c5b50cbc1c70e4dd4e63dbca8f8671c3b51ebdf
imran_buet13 <omeca13@gmail.com>
2019-10-20 22:55:02 +0600

    fixed the details page error

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.blog_home, name='blog_home'),
-    path('details/<int:blog_id>/', views.blog_details, name='blog_details'),
+    path('<int:blog_id>/', views.blog_details, name='blog_details'),
     # path('details/<int: blog_id>/', views.blog_details, name='blog_details'),  If you give space after int: , it will throw an error in the url. So, you must avoid space inside that <>
 ]
